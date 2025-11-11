d62ca6bcb45ef9dc9b1900d02fc7b2531c046c62
Arnaud180296 <1802arkabimbingoy@gmail.com>
2025-11-11 23:46:58 +0100

    bank version 0.2.2 Ajout des interfaces IAccount et IBankAccount ainsi que leurs implementation personelle.

---
+++ arnaudkabimbingoy/arnaudkabimbingoy/IAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arnaudkabimbingoy
+{
+    internal interface IAccount
+    {
+        double Balance { get; }
+
+        public abstract void Deposit();
+        public abstract void Withdraw();
+    }
+}
